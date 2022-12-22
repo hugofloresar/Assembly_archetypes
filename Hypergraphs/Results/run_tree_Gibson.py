5b634b0f2df94e3e8b8bd89ab71602a735c1e83a
Hugo Flores <sqtr.floresarguedas@gmail.com>
2022-12-22 11:34:19 -0600

    Create run_tree_Gibson.py
    Build the tree of models

---
+++ Hypergraphs/Results/run_tree_Gibson.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 17 07:40:45 2019
+
+@author: cimat
+"""
+
+for pat in ['h','uc']:
+    exec(open("gp_Pareto_tie_arrows.py").read())    
+    exec(open("gp_Pareto_tie_arrows_new.py").read())
+    exec(open("gp_Pareto_purple_arrows.py").read())
